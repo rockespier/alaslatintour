e47970fb2b8c45128a65be6b002e2319c4e8e550
U0O1690 <rramos@consulenti.fideuram.it>
2026-07-21 13:44:51 +0200

    fix enviroment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
   apiUrl: '/api',
-=======
-  apiUrl: 'https://alasglobaltour.gestionaminegocio.com/api',
   turnstileEnabled: true,
   turnstileSiteKey: '0x4AAAAAAD5rFoknFwN8uzRh',
->>>>>>> 63499b5cb280724887472078384569be42d74fa9
 };
